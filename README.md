984d0046feb64f7ed0d5be0bffd1a89b3e001b19
Mike Kazantsev <mk.fraggod@gmail.com>
2012-07-14 21:25:29 +0600

    Initial project metadata

---
+++ README.md
@@ -0,0 +1,4 @@
+django-unhosted: unhosted remoteStorage server implementation as a django app
+--------------------
+
+Under development.
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+pkg_root = os.path.dirname(__file__)
+
+# Error-handling here is to allow package to be built w/o README included
+try: readme = open(os.path.join(pkg_root, 'README.txt')).read()
+except IOError: readme = ''
+
+setup(
+
+	name = 'django-unhosted',
+	version = '12.07.2',
+	author = 'Mike Kazantsev',
+	author_email = 'mk.fraggod@gmail.com',
+	license = 'WTFPL',
+	keywords = 'django unhosted app remoteStorage server cloud silo',
+	url = 'https://github.com/mk-fg/django-unhosted',
+
+	description = 'Unhosted remoteStorage server app for django',
+	long_description = readme,
+
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Environment :: Plugins',
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Information Technology',
+		'Intended Audience :: System Administrators',
+		'License :: OSI Approved',
+		'Natural Language :: English',
+		'Operating System :: POSIX',
+		'Operating System :: Unix',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 2 :: Only',
+		'Topic :: Database :: Front-Ends',
+		'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+		'Topic :: Software Development :: Libraries :: Python Modules' ],
+
+	install_requires = ['oauth2app'],
+	packages = find_packages(),
+	package_data = {'': ['README.txt']},
+	exclude_package_data = {'': ['README.*']} )
